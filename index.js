4e9512e9f9f14797699e8b143b8ef097987834af
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-05-08 12:55:02 -0400

    Setup proxy for server to enable API fetching

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
+require('dotenv').config()
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const app = express();
 const PORT = 5000;
 const cors = require('cors')
 
 app.use(cors())
 
-// Middleware to check for API key
-const checkApiKey = (req, res, next) => {
+app.get('/api/api-key', (req, res) => {
+    res.json({ apiKey: process.env.BACKEND_API_KEY });
+  });
+  
+  // Middleware to check for API key
+  const checkApiKey = (req, res, next) => {
     const apiKey = req.header('X-API-KEY');
-    if (!apiKey || apiKey !== process.env.API_KEY) {
+    if (!apiKey || apiKey !== process.env.BACKEND_API_KEY) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
     next();
   };
 
-app.get('/api/exercises', checkApiKey, (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-});
+  // Proxy middleware configuration
+app.use(
+    '/api/exercises',
+    createProxyMiddleware({
+      target: 'https://api.api-ninjas.com',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/exercises': '/v1/exercises',
+      },
+    })
+  );
   
+  // Route handler for the root path
+app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
