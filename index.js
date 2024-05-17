bbc536c3d0048b18affae135cbb4c557f8f5d9f3
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-05-17 08:21:23 -0400

    added endpoints for 2nd api

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 const apiKey = process.env.API_KEY
+const apiKey2 = process.env.API_KEY_E
 
 app.use(cors())
 
-// endpoint for api key
+// endpoint for api key 1
 app.get('/api/api-key', async (req, res) => {
   res.json({ apiKey: apiKey })
 })
 
-// endpoint for exercises
+// endpoint for exercises api-ninja
 app.get('/api/exercises', async (req, res) => {
   const cacheKey = 'exerciseData'
   const cachedData = cache.get(cacheKey)
@@ -39,6 +40,42 @@ app.get('/api/exercises', async (req, res) => {
         console.error("Failed to fetch data:", error);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
+})
+
+
+// endpoint for api key 2
+app.get('/api/api-key2', async (req, res) => {
+  res.json({ apiKey2: apiKey2 })
+})
+
+// endpoint for exercises rapid-api
+app.get('/api/exercises-rapidapi', async (req, res) => {
+  const cacheKey = 'exerciseDataRapidAPI'
+  const cachedData = cache.get(cacheKey)
+
+  if (cachedData && cachedData.muscle === req.query.muscle) {
+    console.log('Serving from cache')
+    return res.json(cachedData)
+  }
+
+  try {
+    const muscle = req.query.muscle
+    console.log("Received muscle:", muscle)
+    const response = await axios ({
+      method: 'GET', 
+      url: 'https://work-out-api1.p.rapidapi.com/search',
+      params: { Muscles: muscle},
+      headers: {
+        'X-RapidAPI-Key': apiKey2,
+        'X-RapidAPI-Host': 'work-out-api1.p.rapidapi.com',
+      },
+    })
+    cache.set(cacheKey, { muscle: muscle, data: response.data })
+    res.json(response.data)
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
 })
 
 // Route handler for the root path
