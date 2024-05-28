8d4ecc65c7916d9f48dbe5623c02b5528779bd53
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-05-28 09:13:04 -0400

    Added CORS middleware to allow reqs from frontend

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 const apiKey = process.env.API_KEY
 const apiKey2 = process.env.API_KEY_E
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://htrain-backend-kxdxjdw3v-hammads-projects-216b65c7.vercel.app/',
+  methods: ['GET', 'POST'],
+  credentials: true, 
+}))
 
 
 // endpoint for api key 1
